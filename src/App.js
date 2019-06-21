8193d0ea0c9c2a3d8d01cdb08171d0fc21b291a7
Diwash Tamang <mtnekros@gmail.com>
2019-06-21 09:58:15 +0545

    Fixed the conflict in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React from 'react';
 import './scss/style.scss';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-<<<<<<< HEAD
 import AddLayer from './components/AddLayer'              
-=======
-import AddLayer from './components/AddLayer'
->>>>>>> a95c878de9fc5b772f8f18454728925c86d84085
+
 
 function App() {
   return (
